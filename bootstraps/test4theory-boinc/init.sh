e558ba7af3ef4416006411305a5bed5ad43050c2
Ioannis Charalampidis <ioannis.charalampidis@cern.ch>
2015-02-03 19:26:12 +0100

    Using different approach on the dumbq-log

--- bootstraps/test4theory-boinc/init.sh
+++ bootstraps/test4theory-boinc/init.sh
@@ -7,10 +7,12 @@
 # Configuration parameters
 BOOTSTRAP_NAME="test4theory-boinc"
 BOOTSTRAP_VER="8"
+JID_VER="-g"
 
 # Less important parameters
 BOOTSTRAP_DIR="/cvmfs/sft.cern.ch/lcg/external/dumbq/bootstraps/${BOOTSTRAP_NAME}"
 T4T_WEBAPP_TGZ="/cvmfs/sft.cern.ch/lcg/external/cernvm-copilot/share/t4t-webapp.tgz"
+DUMBQ_LOG_BIN="/cvmfs/sft.cern.ch/lcg/external/dumbq/bin/dumbq-log"
 
 #
 # Parses the USER_DATA value and retrieves the USER_ID.
@@ -35,14 +37,21 @@ get_user_id()
   #fi
 }
 
-VERSION="7"
+# 1) Start required services
+# ----------------------------------
 
 # Start cron
 service cron start
 
+# 2) Install the test4theory app
+# ----------------------------------
+
 # Unzip the t4t-webapp
 T4T_WEBAPP_DST=/var/www/html
 /bin/tar zxvf $T4T_WEBAPP_TGZ -C $T4T_WEBAPP_DST > /dev/null 2>&1
+
+# 3) Install required binaries
+# ----------------------------------
 
 # Copy the config and debug info scripts to /usr/bin
 cp ${BOOTSTRAP_DIR}/bin/copilot-debug-info /usr/bin
@@ -52,72 +61,77 @@ chmod a+rx /usr/bin/copilot-debug-info
 chmod a+rx /usr/bin/copilot-config
 chmod a+rx /usr/bin/readFloppy.pl
 
+# 4) Cache and prepare Jabber ID
+# ----------------------------------
+
 # BOINC User ID Cache
-BOINC_USER_ID_CACHE="/var/lib/copilot-agent-uuid-saved.$VERSION"
+BOINC_USER_ID_CACHE="/var/lib/copilot-agent-uuid-saved.$BOOTSTRAP_VER"
 if [ -f $BOINC_USER_ID_CACHE ]; then
   . $BOINC_USER_ID_CACHE # 2>&1 >/dev/null
 fi
 
+# If we don't have a cached jabber ID, calculate one
 if [ -z "$AGENT_JABBER_ID" ]; then
+
   # BOINC Config
   BOINC_SERVER=https://lhcathome2.cern.ch
   BOINC_PROJECT=test4theory
 
-  DEV_BOINC_SERVER=https://boinc-dev.cern.ch
-  DEV_BOINC_PROJECT=testing
-
+  # Count number of CPUS
   N_CPU=$(cat /proc/cpuinfo |grep processor|wc -l)
-  REV="-g"
 
+  # Read UserID from floppy
   USER_ID=$(/cvmfs/sft.cern.ch/lcg/external/cernvm-copilot/bin/readFloppy.pl | grep "^USER_ID" | awk -F'=' '{print $2}')
+
+  # If we have a USERID field (challenge mode), use the one provided
   if [ ! -z "${USER_ID}" ]; then
 
-    # Use generic user ID
+    # Use specified UserID
     USER_ID="g-${USER_ID}"
 
   else
 
+    # Otherwise extract BOINC_AUTHENTICATOR and BOINC_USER_ID
     BOINC_AUTHENTICATOR=$(/cvmfs/sft.cern.ch/lcg/external/cernvm-copilot/bin/readFloppy.pl| grep BOINC_AUTHENTICATOR | awk '{split ($0, auth, "="); print auth[2]}')
     BOINC_USERID=$(/cvmfs/sft.cern.ch/lcg/external/cernvm-copilot/bin/readFloppy.pl| grep BOINC_USERID | awk '{split ($0, auth, "="); print auth[2]}')
 
-    #if [ -n "$BOINC_AUTHENTICATOR" -a -n "$BOINC_USERID" ]; then
+    # If we have a BOINC authenticator, get USER_ID from there
     if [ -n $BOINC_AUTHENTICATOR ]; then
       USER_DATA=$(curl $BOINC_SERVER/$BOINC_PROJECT'/am_get_info.php?account_key='$BOINC_AUTHENTICATOR -k -s)
     fi
     get_user_id
 
-    # (Dev server is not working)
-    #if [ "$USER_ID" == "s-" ] ; then
-    #  # Try the dev server
-    #  if [ -n $BOINC_AUTHENTICATOR ]; then
-    #    USER_DATA=$(curl $DEV_BOINC_SERVER/$DEV_BOINC_PROJECT'/am_get_info.php?account_key='$BOINC_AUTHENTICATOR -k -s)
-    #  fi
-    #  USER_ID=""
-    #  get_user_id
-    #
-    #  #indicate that we tried the dev server
-    #  USER_ID="d-""$USER_ID"
-    #  BOINC_USERID="d-""$BOINC_USERID"
-    #fi
-
   fi
 
-  export AGENT_JABBER_ID="agent_""$USER_ID"_"$N_CPU"_"$VERSION"_"$(uuidgen)"_"$REV"
+  # Setup jabber ID and place on cache
+  export AGENT_JABBER_ID="agent_""$USER_ID"_"$N_CPU"_"$BOOTSTRAP_VER"_"$(uuidgen)"_"$JID_VER"
   echo "export AGENT_JABBER_ID=$AGENT_JABBER_ID" > $BOINC_USER_ID_CACHE
-
   chmod -w $BOINC_USER_ID_CACHE
+
 fi
 
+# 5) Prepare user interface
+# ----------------------------------
 
 cp $BOINC_USER_ID_CACHE /var/www/html/logs
 cp /var/log/start-perl-copilot.log /var/www/html/logs
 
-/cvmfs/sft.cern.ch/lcg/external/cernvm-copilot/bin/copilot-config --user-data 2>&1 >/dev/null # to create the ~/copilot-user-data file
+# 6) Start multicolored log
+# ----------------------------------
 
-#if  [ $[ ( $RANDOM % 2 ) ] == 0 ]; then
+${DUMBQ_LOG_BIN} \
+  /var/log/start-perl-copilot.log[green] \
+  /var/log/messages[red] \
+  /var/log/copilot-config[red] \
+  > /dev/tty1
+
+# 7) Start Co-Pilot from CVMFS
+# ----------------------------------
+
+# Create the ~/copilot-user-data file
+/cvmfs/sft.cern.ch/lcg/external/cernvm-copilot/bin/copilot-config --user-data 2>&1 >/dev/null 
+
+# Select configuration and start co-pilot
 export COPILOT_CONFIG=/cvmfs/sft.cern.ch/lcg/external/cernvm-copilot/etc/copilot
-#else
-#  export COPILOT_CONFIG=/cvmfs/sft.cern.ch/lcg/external/experimental/cernvm-copilot/etc/copilot.new
-#fi
-
 /bin/env PATH=$PATH LANG=C  perl -I /cvmfs/sft.cern.ch/lcg/external/cernvm-copilot/lib/perl5/site_perl/5.8.8/ /cvmfs/sft.cern.ch/lcg/external/cernvm-copilot/bin/copilot-agent
+
--- client/dumbq-log.py
+++ client/dumbq-log.py
@@ -19,6 +19,7 @@
 #
 
 import threading, Queue, subprocess
+import datetime
 import time
 import os
 import sys
@@ -34,65 +35,120 @@ TTY_COLORS = {
 	"white": 7
 }
 
-def follow_thread(filename, queue):
-	"""
-	A thread that follows the contents of file 'filename'
-	and feeds it the Queue given in the queue argument
-	"""
-	while True:
-
-		# Wait until the file (re-)appears
-		if not os.path.isfile(filename):
-			time.sleep(0.5)
-			continue
-
-		# Tail file until the tail process dies for any reason
-		p = subprocess.Popen(["tail", "-F", filename], stdout=subprocess.PIPE)
-		while True:
-			line = p.stdout.readline()
-			queue.put(line.rstrip('\n'))
-			if not line:
-				break
-
-def tty_color(message, fore="red", back=None, bold=False):
+def tty_color(message, fore=None, back=None, bold=None):
 	"""
 	Append ANSI colors on the given message and return
 	"""
+
+	# Prepare attributes
+	attr = []
 	# Get foreground color
-	attr = [ str(30 + TTY_COLORS[fore.lower()]) ]
+	if fore != None:
+		attr = [ str(30 + TTY_COLORS[fore.lower()]) ]
 	# Get background color
 	if back != None:
 		attr.append(str(40 + TTY_COLORS[back.lower()]))
 	# Check for bold
-	if bold:
+	if bold == True:
 		attr.append('1')
-	# Return ANSI Escape
-	return "\x1b[%sm%s\x1b[0m" % (';'.join(attr), message)
 
-class PrettyPrinter:
+	# If we have no attributes, don't use ANSI encoding
+	if len(attr) == 0:
+		return message
+	else:
+		# Return ANSI Escape
+		return "\x1b[%sm%s\x1b[0m" % (';'.join(attr), message)
+
+class TailMonitor(threading.Thread):
+
+	def __init__(self, filename, fore, back, bold, prefix):
+		threading.Thread.__init__(self)
+
+		# Keep variables
+		self.filename = filename
+		self.fore = fore
+		self.back = back
+		self.bold = bold
+		self.prefix = prefix
+
+		# Allocate new queue
+		self.queue = Queue.Queue(maxsize=10)
+
+		# Local properties
+		self.active = True
+		self.p = None
+
+	def stop(self):
+		"""
+		Interrupt thread by stopping the forked Popen
+		and resetting the active flag
+		"""
+
+		# Disable
+		self.active = False
+
+		# Kill sub process
+		if self.p != None:
+			self.p.terminate()
+
+
+	def run(self):
+		"""
+		A thread that follows the contents of file 'filename'
+		and feeds it the Queue given in the queue argument
+		"""
+		while self.active:
+
+			# Wait until the file (re-)appears
+			if not os.path.isfile(self.filename):
+				time.sleep(0.5)
+
+				# Stop when interrupted
+				if not self.active:
+					return
+				continue
+
+			# Tail file until the tail process dies for any reason
+			self.p = subprocess.Popen(["tail", "-F", self.filename], stdout=subprocess.PIPE)
+			while True:
+				line = self.p.stdout.readline()
+				self.queue.put(line.rstrip('\n'))
+				if not line:
+					break
+
+			# Reset p value
+			self.p = None
+
+
+class TailList:
 
 	def __init__(self):
-		self.queues = []
+		self.monitors = []
+		self.prefix_str = ""
 
-	def add(self, filename, color="red", bold=False, suffix=""):
+	def add(self, filename, col_fore=None, col_back=None, bold=None, prefix=None):
 		"""
 		Add a new file to monitor with the given color and font information
 		"""
 
-		# Create a new queue
-		q = Queue.Queue(maxsize=10)
+		# Put a new monitor in the list
+		self.monitors.append( 
+				TailMonitor(filename, col_fore, col_back, bold, prefix )
+			)
 
-		# Start a new thread
-		t = threading.Thread(target=follow_thread, args=(filename,q))
-
-		# Put configuration in the queues array
-		self.queues.append(( q, t, color, bold, suffix ))
-
-	def suffix(self):
+	def prefix(self, name):
 		"""
-		Return global suffix
+		Return global prefix
 		"""
-		return ""
+
+		# Replace optional '%n'
+		prefix = self.prefix_str.replace("%n", name)
+
+		# Return prefix only if we have one
+		if not prefix:
+			return ""
+		else:
+			return datetime.datetime.now().strftime(prefix)
 
 	def start(self):
 		"""
@@ -100,29 +156,30 @@ class PrettyPrinter:
 		"""
 
 		# Start threads
-		for q in self.queues:
-			q[1].start()
+		for q in self.monitors:
+			q.start()
 
 		# Iterate over all queues infinitely
 		while True:
 
 			# Protect against exceptions
 			try:
-				for q in self.queues:
-
-					# Unpack queue record
-					(q, t, color, bold, suffix) = q
+				for mon in self.monitors:
 
 					# Follow
 					try:
 
 						# Try to get a line
-						line = q.get(False)
+						line = mon.queue.get(False)
 
-						# Compile suffixes and line color and print
-						print tty_color("%s%s%s" % (self.suffix(), suffix, line), fore=color, bold=bold)
+						# Combine prefixes, line color and print
+						print tty_color(
+								"%s%s%s" % (self.prefix(os.path.basename(mon.filename)), mon.prefix, line), 
+								fore=mon.fore, back=mon.back, bold=mon.bold
+							)
 
 					except Queue.Empty:
+						# No new line, that's ok...
 						pass
 
 				# Sleep a bit
@@ -131,11 +188,109 @@ class PrettyPrinter:
 			except KeyboardInterrupt:
 				# Exit
 				print "Interrupted"
+
+				# Interrupt all tail subprocesses
+				for mon in self.monitors:
+					mon.stop()
+
+				# And exit with error
 				sys.exit(1)
 
 
-# Create a PrettyPrinter
-pp = PrettyPrinter()
-pp.add("/var/log/messages")
-pp.add("/tmp/test", color="green")
+# Create a TailList
+pp = TailList()
+
+# Display error message one various help/wrong questions
+if (len(sys.argv) == 1) or ('--help' in sys.argv) or ('help' in sys.argv) or ('-h' in sys.argv):
+	print "DumbQ Logging Helper"
+	print "Usage:"
+	print ""
+	print " dumbq-log [--prefix=""] <filename>[<attr>,...]"
+	print ""
+	print "The accepted options are:"
+	print ""
+	print " --prefix : Specify the prefix for every line (check strftime python"
+	print "            function reference for accepted parameters."
+	print "            Additionally \%n expands to file basename."
+	print ""
+	print "Attr can be one or more of:"
+	print ""
+	print "    color : The color to use on the lines of this log file."
+	print "     bold : Where to use bold letters for this font."
+	print ""
+	print "Example:"
+	print ""
+	print " dumbq-log \\"
+	print "   /var/log/messages[red] \\"
+	print "   /var/log/boot.log[cyan,bold]"
+	print ""
+	sys.exit(0)
+
+# Handle commands
+for f in sys.argv[1:]:
+
+	# === OPTION ==========
+
+	# Check if this is an option
+	if f[0] == "-":
+
+		# Check for prefix
+		if f[0:9] == "--prefix=":
+			pp.prefix_str = f[9:]
+
+		# Otherwise that's an invalid option
+		else:
+			print "ERROR: Unrecognized option '%s'" % f
+			sys.exit(1)
+
+		# Continue loop
+		continue
+
+	# === FILE ============
+
+	# Split file/attribute
+	parts = f.split("[")
+	if (len(parts) == 1):
+		print "ERROR: Missing attribute for '%s'" % f
+		sys.exit(1)
+
+	# Get file
+	f_name = parts[0]
+
+	# Validate attribute
+	if parts[1][-1] != ']':
+		print "ERROR: Missing attribute terminator in '%s'" % f
+		sys.exit(1)
+
+	# Get attributes
+	f_attr = parts[1][:-1].split(",")
+
+	# Parse attributes
+	a_fore = None
+	a_back = None
+	a_bold = None
+	for attr in f_attr:
+		attr = attr.lower()
+
+		# Check for color
+		if attr in TTY_COLORS:
+			a_fore = attr
+
+		# Check for background-color
+		elif (attr[0:2] == "b-") and (attr[2:] in TTY_COLORS):
+			a_back = attr[2:]
+
+		# Check for bold
+		elif attr == "bold":
+			a_bold = True
+
+		# Otherwise that's an error
+		else:
+			print "ERROR: Invalid attribute '%s' in '%s'" % (attr, f)
+			sys.exit(1)
+
+	# Add file
+	pp.add(f_name, a_fore, a_back, a_bold)
+
+# Start tracing the logs specified
 pp.start()
