1378886cc3209b6e3940343b6a3803e19f2b02c5
jvican <jorgevc@fastmail.es>
2015-09-01 18:16:45 +0200

    dumbq-client: add utils
    * Add context managers to remove redundant behaviour

--- client/utils/utils.py
+++ client/utils/utils.py
@@ -1,8 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# DumbQ 2.0 - A lightweight job scheduler
+# Copyright (C) 2015-2016  Jorge Vicente Cantero, CERN
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+
 import errno
 import os
 import json
 
 from contextlib import contextmanager
+
+DEVNULL = os.open(os.devnull, os.O_RDWR)
 
 
 def error_and_exit(error_message, logger):
@@ -17,7 +38,13 @@ def ignored(*exceptions):
     except exceptions:
         pass
 
-# TODO Create a loggable exception handler
+
+@contextmanager
+def logged(logger, message, *exceptions):
+    try:
+        yield
+    except exceptions:
+        logger.warning(message)
 
 
 def create_dir_if_nonexistent(dirpath, mode=0777):
